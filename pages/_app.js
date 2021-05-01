40df2de5dcd178d46b6d52c096db13f8834eb29d
nguyenvietkhoi <nguyenvietkhoi90@gmail.com>
2021-05-01 22:30:34 +0700

    dict

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/linguaevum.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -3,61 +3,53 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{background: "url(/wave.png)"}}>
       <Head>
-        <title>Create Next App</title>
+        <title>Linguaevum</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="http://linguaevum.ddns.net/">Linguaevum</a> xin kính chào
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://nguyenvietkhoi.github.io/SinicIME/" className={styles.card}>
+            <h3>Sinic IME</h3>
+            <p>Bộ gõ chữ Hán Nôm</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="/TayNung" className={styles.card}>
+            <h3>Từ điển Tày Nùng - Việt</h3>
+            <p>Tra cứu từ ngữ tiếng Tày Nùng</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://nguyenvietkhoi.github.io/SinicIME/TaiDB"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Dữ liệu Kra-Dai</h3>
+            <p>Cập nhật cơ sở dữ liệu ngữ hệ Kra-Dai</p>
+          </a>
+
+          <a
+            href="https://nguyenvietkhoi.github.io/SinicIME/AustroasiaticDB"
+            className={styles.card}
+          >
+            <h3>Dữ liệu Nam Á</h3>
+            <p>Cập nhật cơ sở dữ liệu ngữ hệ Nam Á</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          © 2021{' '}
+          <img src="/favicon.ico" alt="Vercel Logo" className={styles.logo} />
+			  &nbsp;Linguaevum
         </a>
       </footer>
     </div>
