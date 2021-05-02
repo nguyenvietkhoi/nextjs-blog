2f62944673f0bb857ecf45bdd054e1d2c5394125
nguyenvietkhoi <nguyenvietkhoi90@gmail.com>
2021-05-02 21:35:31 +0700

    dict

--- pages/TayNung.js
+++ pages/TayNung.js
@@ -1,25 +1,27 @@
 import Head from 'next/head'
+import React, { useEffect, useState } from "react";
 import styles from '../styles/Home.module.css'
 import { taynung } from "../data.js";
 
 var words = null; 
 
 
-const SearchBar = () => (
-    <div style={{padding: "5px 0"}}>
+const SearchBar = (dict) => (
+	<>
         <input className={["textpad", styles.box].join(' ')} style={{color: "#000", maxWidth: "220px"}}
             type="text"
             id="txtPad"
             placeholder="Từ cần tra..."
             name="ori" 
         />
-        <button onClick={lookupOri} className={styles.button}>Tra</button>
-    </div>
+        <button onClick={(evt) => lookupOri(dict, evt)} className={styles.button}>Tra</button>
+	</>
 );
-const lookupOri = async (event) => {
+const lookupOri = async (dict, event) => {
 	event.preventDefault();
     var orireq = document.getElementById("txtPad").value;
-    const req = await fetch('./api/taynung/' + orireq);
+	var dictnumber = dict?"2":"1";
+    const req = await fetch('./api/taynung/' + dictnumber +'/'+ orireq);
     words = await req.json();	
 
 if (words != null) {
@@ -40,21 +42,51 @@ for (var i=0; i<words.length; i++) {
 
 
 export default function Home() {
+  const [showMe, setShowMe] = useState(false);
+  const [dict2, setDict2] = useState(false);
+  var dict = false;
+  var dictname1 = "Tày Nùng → Việt";
+  var dictname2 = "Việt → Tày Nùng";
+  function toggle(){
+    setShowMe(!showMe);
+  }
+  function inverse(){
+    setDict2(!dict2);
+  }
   return (
   <>
     <div className={styles.container}>
       <Head>
         <title>TĐ Tày Nùng</title>
+      </Head>
         <link rel="icon" href="/favicon.ico" />
 		
-        <h3 className={styles.title} style={{margin: "1rem", fontSize: "30px", color: "#DDC326"}}>
-          Từ điển Tày Nùng - Việt
+        <h3 className={styles.title} style={{margin: "2rem", fontSize: "30px", color: "#DDC326"}}>
+          Từ điển <box className={styles.button} onClick={inverse} style={{background: "#232323", fontSize: "30px", color: "#eee"}}><a>{dict2?dictname2:dictname1}</a></box>
         </h3>
-		
-      </Head>
 
-		  {SearchBar()}
-			  
+    <div style={{padding: "5px 0"}}>
+		  {SearchBar(dict2)}
+			  <button onClick={toggle} className={styles.button} style={{fontSize: "14px"}}>⌨️</button>
+    </div>
+		<div style={{margin: "0",display: showMe?"block":"none"}}>
+<table className={"textpad"}>
+  <tr>
+    <td></td><td>ă</td><td>â</td><td>đ</td><td>ê</td><td>j</td><td>z</td><td>ô</td><td>ơ</td><td>f</td><td>ư</td><td>đ</td>
+  
+    <td>à</td><td>ằ</td><td>ầ</td><td>è</td><td>ề</td><td>ì</td><td>ò</td><td>ồ</td><td>ờ</td><td>ù</td><td>ừ</td><td>ỳ</td>
+  </tr><tr>
+    <td>á</td><td>ắ</td><td>ấ</td><td>é</td><td>ế</td><td>í</td><td>ó</td><td>ố</td><td>ớ</td><td>ú</td><td>ứ</td><td>ý</td>
+  
+    <td>a̱</td><td>ă̱</td><td>â̱</td><td>e̱</td><td>ê̱</td><td>i̱</td><td>o̱</td><td>ô̱</td><td>ơ̱</td><td>u̱</td><td>ư̱</td><td>y̱</td>
+  </tr><tr>
+    <td>ả</td><td>ẳ</td><td>ẩ</td><td>ẻ</td><td>ể</td><td>ỉ</td><td>ỏ</td><td>ổ</td><td>ở</td><td>ủ</td><td>ử</td><td>ỷ</td>
+  
+    <td>ạ</td><td>ặ</td><td>ậ</td><td>ẹ</td><td>ệ</td><td>ị</td><td>ọ</td><td>ộ</td><td>ợ</td><td>ụ</td><td>ự</td><td>ỵ</td>
+  </tr>
+</table>
+		</div>
+		
       <main className={styles.main}>   	
 	<div id="maincontent"></div>
       </main>
---
+++ pages/api/taynung/[dict]/[ori].ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from 'next' ;
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3')
+const {open} = require('sqlite')
+
+async function openDB (){
+    return open({
+        filename : './taynungdict.db',
+        driver: sqlite3.Database
+    })
+}
+
+export default async function getByOri(req: NextApiRequest, res: NextApiResponse) {
+    const db = await openDB();
+
+	var sql;
+	var dict = req.query.dict;
+	var cont = req.query.ori;
+	if (dict == "1")
+		sql = 'SELECT * FROM taynung WHERE ori = \'' + cont + '\'' ;
+	else
+		sql = 'SELECT * FROM taynung WHERE viet LIKE \'% ' + cont + ' %\' OR viet LIKE \'' + cont + ' %\' OR viet LIKE \'% ' + cont + '\' OR viet = \'' + cont + '\'' ;
+
+	const rows = await db.all(sql);
+	console.log(JSON.stringify(rows));
+
+	var gloss = [];
+rows.forEach(function(word){
+	var newgloss = {
+		nom: (word.nom!=null) ? word.nom : "",
+		ori: (word.ori!=null) ? word.ori : "",
+		ety: (word.ety!=null) ? word.ety : "",
+		viet: (word.viet!=null) ? word.viet : "",
+		meanings : []
+	};
+	var means = word.meaning.split("—");
+	means.forEach(function(mean){
+		var explaination = mean.split("⇨");
+		var examples = [];
+		if (explaination.length > 1) {
+			for (var i=1; i<explaination.length; i++) {
+				var example = explaination[i].split(":");
+				examples.push({oriEg: example[0], vietEg: example[1]});
+			}
+		}
+		newgloss.meanings.push({mean: explaination[0], examples: examples});
+	});
+	gloss.push(newgloss);
+});	
+	res.status(200).json(gloss);	
+}
